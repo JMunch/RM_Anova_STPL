57f2b07dbc576bb833f06d8d866f4f22664186c9
Constantin <Constantin>
2016-12-05 21:11:05 +0100

    So, ich hab alles nochmal etwas hübsch gemacht vor morgen...

--- r/quantlet6_test_and_adjustment_for_sphericity.R
+++ r/quantlet6_test_and_adjustment_for_sphericity.R
@@ -1,85 +1,95 @@
-##### Mauchly's Test of Sphericity and correction factors (epsilon)
-# To conduct the Mauchly's Test of Sphericity (Mauchly, 1940), the procedure described by Huynh and Feldt (1970) is used
-
-# I was sure how to partition the codes into function or should it be one function??????????????????????????? JM
-
+##### Mauchly's Test of Sphericity and the appropriate correction factors (epsilon)
+  # Function 'rma' is required (quantlet1_rm_anova.R)!
+  # To conduct the Mauchly's Test of Sphericity (Mauchly, 1940), the procedure described by Huynh and Feldt (1970) is used
 
 
-mauchly = function(rma_data){
+rma_sphericity_test = function(rma_data, append = FALSE){
   
   
 # Defining some variables -------------------------------------------------
 
   
+  # number of factor levels
   k = length(rma_data[1,]) - 1
+  
+  # number of entities
   n = as.numeric(length(rma_data[,1]))
-  maxpoly = k - 1
+  
+  # Factor degrees of freedom
+  df = k - 1
 
-# Empirical covariance matrix
+  # Empirical covariance matrix
   covariance_matix = cov(as.matrix(rma_data[,-1]))
 
   
-# Helmert matrix required for the computation of mauchly's W ---------------
+# Helmert matrix required for the computation of mauchly's W --------------
 
 
   helmert = function(k){
           H = matrix(0, k, k)
-          diag(H) = (0:(k - 1)) * (-((0:(k - 1)) * ((0:(k - 1)) + 1))^(-0.5))
+          diag(H) = (0:df) * (-((0:df) * ((0:df) + 1))^(-0.5))
           for (i in 2:k){
                   H[i,1:(i - 1)] = ((i - 1)*(i))^(-0.5)
           }
-  #       H[1,] = 1/sqrt(k)
-  #       The first row of the helmert matrix is not required for further use in this procedure
+  #        H[1,] = 1/sqrt(k)
           return(H)
   }
 
-
-C = helmert(k)[-1,]
+  # The first row of the helmert matrix is not required for further use in this procedure
+  C = helmert(k)[-1,]
 
 
 # Computation of mauchly's W ----------------------------------------------
 
 
-  w = det(C%*%covariance_matix%*%t(C)) / ((sum(diag(C%*%covariance_matix%*%t(C))) / maxpoly)^(maxpoly))
+  w = det(C%*%covariance_matix%*%t(C)) / ((sum(diag(C%*%covariance_matix%*%t(C))) / df)^(df))
 
 
-# Chi-Square Test ------------------------------------------
+# Chi-Square Test ---------------------------------------------------------
 
 
-# Computing the degrees of freedom for the chi-square value 
-  df_w = ((k * (k - 1)) / 2) - 1
+  # Computing the degrees of freedom for the chi-square value 
+  df_w = ((k * df) / 2) - 1
 
-# Computing the chi-square value
-  f = 1 - ((2 * ((k - 1)^2) + (k - 1) + 2) / (6 * (k - 1) * (n - 1)))
+  # Computing the chi-square value
+  f = 1 - ((2 * (df^2) + df + 2) / (6 * df * (n - 1)))
   chi_sq_w = -(n - 1) * f * log(w)
 
-# Computing the corresponding p-value
+  # Computing the corresponding p-value
   p_w = 1 - pchisq(chi_sq_w, df_w)
 
 
 # Create summary table for mauchly's test ---------------------------------
 
   
-  mauchly_table = data.frame("Source" = "Factor",
-                                "Mauchly's W" = w,
-                                "Chi square" = chi_sq_w,
-                                "df" = df_w,
-                                "p" = p_w
+  mauchly_table = data.frame(check.names = FALSE,
+                             "Source" = "Factor",
+                             "Mauchly's W" = w,
+                             "Chi square" = chi_sq_w,
+                             "df" = df_w,
+                             "p" = p_w
   )
   rownames(mauchly_table) = NULL
-  
-  
-  
+
+
+# The three different correction factors (epsilon) ------------------------
   # Computing the three different correction factors for the nominator/denominator df of the rmANOVA factor F-test (Box, 1954a; Box, 1954b; Geisser & Greenhouse, 1958; Greenhouse & Geisser, 1959; Huynh & Feldt, 1976) )
+
+    
+  # Lower-Bound correction (Greenhouse & Geisser, 1959)
+  epsilon_lb = 1 / df
   
-  epsilon_lb = 1 / (k - 1)
+  # Box correction (Geisser & Greenhouse, 1958)
+  epsilon_gg = (sum(diag(C%*%covariance_matix%*%t(C)))^2) / (df * sum(diag(t((C%*%covariance_matix%*%t(C)))%*%(C%*%covariance_matix%*%t(C)))))
   
-  epsilon_gg = (sum(diag(C%*%covariance_matix%*%t(C)))^2) / (maxpoly * sum(diag(t((C%*%covariance_matix%*%t(C)))%*%(C%*%covariance_matix%*%t(C)))))
+  # Huynh-Feldt correction (Huynh & Feldt, 1976)
+  epsilon_hf = min((n * df * epsilon_gg - 2) / (df * (n - 1) - (df^2 * epsilon_gg)), 1)
   
-  epsilon_hf = min((n * maxpoly * epsilon_gg - 2) / (maxpoly * (n - 1) - (maxpoly^2 * epsilon_gg)), 1)
   
+# Computing the adjusted p-values for the ANOVA-factor F-test -------------
+  # The adjustment is realised via correction of the F-distribution degrees of freedom by multplication with the respective correction factor (epsilon)
+  # rmANOVA function 'rma' is required!  
   
-  # Computing the adjusted p-values for the ANOVA-factor F-test (rmANOVA function 'rma' is required)  
   
   ANOVA_table = rma(rma_data)
   
@@ -96,51 +106,56 @@ C = helmert(k)[-1,]
   p_factor_hf = 1 - pf(ANOVA_table[2,5], corrected_factor_df, corrected_error_df)
   
   
-  # Table with adjusted p-values
+# Table with adjusted p-values and respective corection factors -----------
   
-  epsilon_table = data.frame("Source" = c("Epsilon", "Adjusted p-Value"),
+  
+  epsilon_table = data.frame(check.names = FALSE,
+                             "Source" = c("Epsilon", "Adjusted p-Value"),
                              "Lower-Bound correction (Greenhouse & Geisser, 1959)" = c(epsilon_lb, p_factor_lb),
                              "Box correction (Geisser & Greenhouse, 1958)" = c(epsilon_gg, p_factor_gg),
                              "Huynh-Feldt correction (Huynh & Feldt, 1976)" = c(epsilon_hf, p_factor_hf)
   )
   rownames(epsilon_table) = NULL
+
   
-  
-  # Coose recomendet adjustment and add to ANOVA-table    
-  
+# Coose recomendet adjustment and add to ANOVA-table ----------------------
+  # Correction is only applied if assumption of sphericity is rejected 
+  # Recomendation based on results of Greenhouse and Geisser (1959) as well as Huynh and Feldt (1976)
+
+    
   if (p_w < .05){
     if (p_factor_lb < .05){
       ANOVA_table[,"Recommended Lower-Bound corrected p-Value (Greenhouse & Geisser, 1959))"] = c(NA, p_factor_lb, NA, NA, NA, NA)
     }else{
-      if (p_factor_lb > .05){
-        if (epsilon_gg < .75){
-          ANOVA_table[,"Recommended Box corrected p-Value (Geisser & Greenhouse, 1958)"] = c(NA, p_factor_gg, NA, NA, NA, NA)
-        }else{
-          ANOVA_table[,"Recommended Huynh-Feldt corrected p-Value (Huynh & Feldt, 1976)"] = c(NA, p_factor_hf, NA, NA, NA, NA)
-        }
+      if (epsilon_gg < .75){
+        ANOVA_table[,"Recommended Box corrected p-Value (Geisser & Greenhouse, 1958)"] = c(NA, p_factor_gg, NA, NA, NA, NA)
+      }else{
+        ANOVA_table[,"Recommended Huynh-Feldt corrected p-Value (Huynh & Feldt, 1976)"] = c(NA, p_factor_hf, NA, NA, NA, NA)
       }
     }
   }
+
+
+# Return spericity test and p-value adjustment ----------------------------
+  # If append is 'TRUE' the function will also return the ANOVA-table with the recomendet adjustment of the p-value  
   
-  return(list("mauchly_test" = mauchly_table, "corrected_Anova_Table" = ANOVA_table))
+      
+  if (append == TRUE){
+    return(list("mauchly_test" = mauchly_table, "correction_factors_epsilon" = epsilon_table, "corrected_Anova_Table" = ANOVA_table))
+  }else{
+    return(list("mauchly_test" = mauchly_table, "correction_factors_epsilon" = epsilon_table))
+  }
 }
 
 
-
-
-# ----------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Testing:
 
 source("r/simulate_rma_data.R")
 rma_data = sim_rma_data(10, 5)
-mau = mauchly(rma_data)
+sphe = rma_sphericity_test(rma_data, TRUE)
 
-mau$corrected_Anova_Table
-mau$mauchly_test
-# Mauchly correction+
-#mauchly_correction_factors(rma_data)
-
-
-
-
+sphe$mauchly_test
+sphe$correction_factors_epsilon
+sphe$corrected_Anova_Table
 
