e63b5027056b66d39498128afa2003d67f2cb3b7
JMunch <jmunch@outlook.de>
2016-12-11 17:42:26 +0100

    Quantlet 7: - changed variable names

--- r_code/quantlets/q7_simulate_one_way_rmANOVA_data.R
+++ r_code/quantlets/q7_simulate_one_way_rmANOVA_data.R
@@ -1,13 +1,13 @@
 # Function to simulate data for repeated measurement ANOVA
 
 
-sim_rma_data = function(n, k, means = NULL, poly_order = NULL, noice_sd = 10, between_subject_sd = 40, NAs = 0){
+sim_ow_rma_data = function(n, k, means = NULL, poly_order = NULL, noice_sd = 10, between_subject_sd = 40, NAs = 0){
   
 # Create empty n x k matrix 
-rma_data = matrix(, nrow = n, ncol = k + 1)
+ow_rma_data = matrix(, nrow = n, ncol = k + 1)
 
 # Add column with subject_id
-  rma_data[, 1] = 1:n
+ ow_rma_data[, 1] = 1:n
   
 
   if(!is.null(means)){
@@ -38,12 +38,12 @@ rma_data = matrix(, nrow = n, ncol = k + 1)
   }
   
 # Add con_mean to the rma_data matrix
-  rma_data[, 2:(k+1)] = matrix(rep(con_means, each = n), nrow = n)
+  ow_rma_data[, 2:(k+1)] = matrix(rep(con_means, each = n), nrow = n)
   
 # Simulate subject means
 # Calculate the deviation from the conditional mean for each subject
   mean_deviation = rnorm(n, mean = 0, sd = between_subject_sd)
-  rma_data[, 2:(k+1)]  = rma_data[, 2:(k+1)] + mean_deviation
+ ow_rma_data[, 2:(k+1)]  = ow_rma_data[, 2:(k+1)] + mean_deviation
 
 # Check if only one noice_sd was passed (no sphericity) and create vector 
   if(length(noice_sd) == 1){
@@ -62,12 +62,12 @@ rma_data = matrix(, nrow = n, ncol = k + 1)
   for(i in 1:k){
     noice[,i] = rnorm(n, mean = 0, sd = noice_sd[i])
   }
-  rma_data[, 2:(k+1)]  = rma_data[, 2:(k+1)] + noice
+  ow_rma_data[, 2:(k+1)]  = ow_rma_data[, 2:(k+1)] + noice
   
 # Simulating NAs, adds the number of NA to the the data passed as argument
   if(NAs > 0){
     for(i in 1:NAs){
-      rma_data[runif(1, min = 1, max = n), runif(1, min = 2, max = (k + 1))] = NA
+      ow_rma_data[runif(1, min = 1, max = n), runif(1, min = 2, max = (k + 1))] = NA
     } 
   }
 
@@ -77,15 +77,15 @@ rma_data = matrix(, nrow = n, ncol = k + 1)
   for(i in 1:k){
     factor_names[i+1] = paste("Factor",i)
   }
-  colnames(rma_data) = factor_names
+  colnames(ow_rma_data) = factor_names
   
-  return(data.frame(rma_data))
+  return(data.frame(ow_rma_data))
 }
 
 # ------------------------------------------------------------
 # Testing:
 
 source("r/quantlet1_rm_anova.R")
-rma_data = sim_rma_data(1000, 4, means = NULL, poly_order = 5, noice_sd = c(10, 20, 30, 20), between_subject_sd = 40, NAs = 0)
+rma_data = sim_ow_rma_data(1000, 4, means = NULL, poly_order = 2, noice_sd = c(10, 20, 30, 20), between_subject_sd = 40, NAs = 1 )
 rma_data
 
