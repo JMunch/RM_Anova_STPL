f6fc6f2e62eed71c3754c3c6ee9f84aace701f3c
Nikolas Hoeft <nikolas.hoeft@yahoo.de>
2016-12-13 23:37:09 +0100

    - moved definition of n and k to the beginning  - created error message function (stops function if the data does not meet the requirements.  - suppressed warning message of dplyr about masked objects (no useful information for the user)

--- r_code/quantlets/q1_one_way_rmANOVA.R
+++ r_code/quantlets/q1_one_way_rmANOVA.R
@@ -2,23 +2,46 @@
 
 
 ow_rma = function(ow_rma_data){
+    
+    
+    # Define needed constants and the dependent variable ----------------------
+    
+    # Number of entities
+    n = nrow(ow_rma_data)
+    
+    # Number of factor levels 
+    k = ncol(ow_rma_data) - 1
+    
+    dependent_variable = as.matrix(ow_rma_data[, -1])
+    
+    
+    
+    # check if the data meet the requirements ---------------------------------
+
+    
+    # ow_rma_data needs to meet the following requirements:
+    
+    # all variables must be numeric
+    if(all(sapply(ow_rma_data, is.numeric)) == FALSE | any(sapply(ow_rma_data, is.factor))){
+        stop("All variables in ow_rma_data must be numeric")
+    }
+    
+    # n > k (i.e. more entities than factor levels)
+    if(n <= k){
+        stop("Number of entities must exceed number of factor levels")
+    }
+    
+    # k >= 2 (i.e. at least two or more factor levels)
+    if(k < 2){
+        stop("At least two factor factor levels required")
+    }
   
   
 # Libraries needed --------------------------------------------------------
 
-  
-  require(dplyr)
-
-
-# Define needed constants and the dependent variable ----------------------
-
-  # Number of entities
-  n = nrow(ow_rma_data)
-
-  # Number of factor levels 
-  k = ncol(ow_rma_data) - 1
-
-  dependent_variable = as.matrix(ow_rma_data[, -1])
+    # suppress warning message about masked objects by dplyr
+    # NOTE: This function still loads the dplyr package!
+    suppressWarnings(suppressMessages(require(dplyr)))
 
 
 # Define basic anova components -------------------------------------------
