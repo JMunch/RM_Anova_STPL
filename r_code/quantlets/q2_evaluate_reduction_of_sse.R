40eff1ae8af264b9cdd6a1b08c342eedbaa103ad
Nikolas Hoeft <nikolas.hoeft@yahoo.de>
2016-12-14 11:04:55 +0100

    added error functions which stop the function and throw an error message if the data don't meet the requirements

--- r_code/quantlets/q2_evaluate_reduction_of_sse.R
+++ r_code/quantlets/q2_evaluate_reduction_of_sse.R
@@ -3,8 +3,32 @@
 
 
 ow_rma_sse_reduct = function(ow_rma_data, plot_type = "pie"){
+    
+    
+    
+    # check if the data meet the requirements ---------------------------------
+    
+    
+    # ow_rma_data needs to meet the following requirements:
+    
+    # all variables must be numeric
+    if(all(sapply(ow_rma_data, is.numeric)) == FALSE | any(sapply(ow_rma_data, is.factor))){
+        stop("All variables in ow_rma_data must be numeric")
+    }
+    
+    # n > k (i.e. more entities than factor levels)
+    if(nrow(ow_rma_data) <= (ncol(ow_rma_data)-1)){
+        stop("Number of entities must exceed number of factor levels")
+    }
+    
+    # k >= 2 (i.e. at least two or more factor levels)
+    if((ncol(ow_rma_data)-1) < 2){
+        stop("At least two factor factor levels required")
+    }
   
-  
+
+    
+      
 # Libraries needed ----------------------------------------------------------
  
   
@@ -39,6 +63,8 @@ ow_rma_sse_reduct = function(ow_rma_data, plot_type = "pie"){
     n = (k * n_group)
     
     dependent_variable = as.matrix(ow_rma_data[, -1])
+    
+
   
   
 # Define basic ANOVA components ---------------------------------------------
