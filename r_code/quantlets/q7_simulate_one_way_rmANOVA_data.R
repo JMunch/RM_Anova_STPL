20e08f46cfeee5dfe7e972520b9188ec434e4dd3
Constantin <Constantin>
2016-12-16 18:19:51 +0100

    Quantlet 7 - wiederherstellung der änderungen vom 12.12.

--- r_code/quantlets/q7_simulate_one_way_rmANOVA_data.R
+++ r_code/quantlets/q7_simulate_one_way_rmANOVA_data.R
@@ -1,31 +1,35 @@
 # Function to simulate data for repeated measurement ANOVA
 
 
-sim_ow_rma_data = function(n, k, means = NULL, poly_order = NULL, noice_sd = 10, between_subject_sd = 40, NAs = 0){
+sim_ow_rma_data = function(n, k, means = NULL, poly_order = NULL, noise_sd = 10, between_subject_sd = 40, NAs = 0){
   
-# Create empty n x k matrix 
-ow_rma_data = matrix(, nrow = n, ncol = k + 1)
-
-# Add column with subject_id
- ow_rma_data[, 1] = 1:n
   
-
+  # Create data structure and simulate data --------------------------------------------------
+  
+  
+  # Create empty n x k matrix 
+  ow_rma_data = matrix(, nrow = n, ncol = k + 1)
+  
+  # Add column with subject_id
+  ow_rma_data[, 1] = 1:n
+  
+  
   if(!is.null(means)){
     con_means = means
     
-# Check if length of mean vector corresponds to k
+    # Check if length of mean vector corresponds to k
     if(length(means) != k){
       k = length(means)
       print("Number of factors (k) was changed, because the length of means vector and argument k do not correspond.")
     }
-  } else {
+  }else{
     
-# Simulate conditional means
+    # Simulate conditional means
     if(is.null(poly_order)){
-  con_means = runif(k, min = 100, max = 300)
-    } else{
+      con_means = runif(k, min = 100, max = 300)
+    }else{
       
-# Generate polinomial conditional means
+      # Generate polinomial conditional means
       factors = runif((poly_order + 1), min = 0, max = 1)
       x = order(runif(k, min = 100, max = 300), decreasing = FALSE)
       con_means = matrix(factors[1], nrow = k)
@@ -33,45 +37,64 @@ ow_rma_data = matrix(, nrow = n, ncol = k + 1)
       for(p in (2: (poly_order + 1))){
         con_means = con_means + factors[p] * x ^ p 
       }
-      
     }
   }
   
-# Add con_mean to the rma_data matrix
+  # Add con_mean to the rma_data matrix
   ow_rma_data[, 2:(k+1)] = matrix(rep(con_means, each = n), nrow = n)
   
-# Simulate subject means
-# Calculate the deviation from the conditional mean for each subject
+  # Simulate subject means
+  # Calculate the deviation from the conditional mean for each subject
   mean_deviation = rnorm(n, mean = 0, sd = between_subject_sd)
- ow_rma_data[, 2:(k+1)]  = ow_rma_data[, 2:(k+1)] + mean_deviation
-
-# Check if only one noice_sd was passed (no sphericity) and create vector 
-  if(length(noice_sd) == 1){
-  noice_sd = rep(noice_sd, times =  k)
+  ow_rma_data[, 2:(k+1)]  = ow_rma_data[, 2:(k+1)] + mean_deviation
+  
+  # Check if only one noise_sd was passed (no sphericity) and create vector 
+  if(length(noise_sd) == 1){
+    noise_sd = rep(noise_sd, times =  k)
   }
   
-# Error if noice_sd vector not a vector of length k -----------------------
-  if(length(noice_sd) != k){
-    print("The vector passed for noice_sd does not have the length k. Please pass a vector of the length k.")
+  # Error if noise_sd vector not a vector of length k 
+  if(length(noise_sd) != k){
+    print("The vector passed for noise_sd does not have the length k. Please pass a vector of the length k.")
     return(NULL)
   }
   
-
-# Add noice to data
-  noice = matrix(NA, nrow = n, ncol = k) 
-  for(i in 1:k){
-    noice[,i] = rnorm(n, mean = 0, sd = noice_sd[i])
-  }
-  ow_rma_data[, 2:(k+1)]  = ow_rma_data[, 2:(k+1)] + noice
   
-# Simulating NAs, adds the number of NA to the the data passed as argument
+  # Add noise to data ----------------------------------------------------------
+  
+  
+  noise = matrix(NA, nrow = n, ncol = k) 
+  for(i in 1:k){
+    noise[,i] = rnorm(n, mean = 0, sd = noise_sd[i])
+  }
+  ow_rma_data[, 2:(k+1)]  = ow_rma_data[, 2:(k+1)] + noise
+  
+  # Simulating NAs, adds the number of NA to the the data passed as argument
   if(NAs > 0){
     for(i in 1:NAs){
       ow_rma_data[runif(1, min = 1, max = n), runif(1, min = 2, max = (k + 1))] = NA
     } 
   }
-
-# Naming columns
+  
+  
+  # Listwise deletion in case of NAs -------------------------------------------
+  
+  
+  deletionvector = vector(mode="numeric", length=0)
+  
+  for(i in 1:nrow(ow_rma_data)) {
+    if(any(is.na(ow_rma_data[i,])) == TRUE) {
+      deletionvector = union(deletionvector, i)
+      print(paste("Listwise deletion due to missing value(s) for subject", i))
+    }
+  }
+  
+  ow_rma_data = ow_rma_data[-deletionvector,]  
+  
+  
+  # Naming columns ------------------------------------------------------------
+  
+  
   factor_names = character(k+1)
   factor_names[1] = "Subject_id"
   for(i in 1:k){
@@ -82,10 +105,11 @@ ow_rma_data = matrix(, nrow = n, ncol = k + 1)
   return(data.frame(ow_rma_data))
 }
 
-# ------------------------------------------------------------
-# Testing:
 
-source("r/quantlet1_rm_anova.R")
-rma_data = sim_ow_rma_data(1000, 4, means = NULL, poly_order = 2, noice_sd = c(10, 20, 30, 20), between_subject_sd = 40, NAs = 1 )
-rma_data
+# ---------------------------------------------------------------------------
+
+
+# Testing:
+ow_rma_data = sim_ow_rma_data(1000, 4, means = NULL, poly_order = 2, noise_sd = c(10, 20, 30, 20), between_subject_sd = 40, NAs = 1 )
+ow_rma_data
 
