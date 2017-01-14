500c5256bf404b297fcb09dcbbe6deb5170adba5
schreckf <freddi.s@gmx.net>
2017-01-14 21:09:41 +0100

    Präsentation soweit fertig. Nur Code ggf noch einfügen

--- r_code/script_for_presentation.R
+++ r_code/script_for_presentation.R
@@ -1,34 +1,35 @@
 # load packages
+install.packages("xtable")
 library(xtable)
 
 # Load all quantlets
-source("r_code/quantlets/q1_one_way_rmANOVA.R")
-source("r_code/quantlets/q2_evaluate_reduction_of_sse.R")
-source("r_code/quantlets/q3_test_and_adjustment_for_sphericity.R")
-source("r_code/quantlets/q4_adjusted_and_unadjusted_CI.R")
-source("r_code/quantlets/q5_effect_size.R")
-source("r_code/quantlets/q6_orthogonal_polynomial_contrasts.R")
-source("r_code/quantlets/q7_simulate_one_way_rmANOVA_data.R")
+source("quantlets/q1_one_way_rmANOVA.R")
+source("quantlets/q2_evaluate_reduction_of_sse.R")
+source("quantlets/q3_test_and_adjustment_for_sphericity.R")
+source("quantlets/q4_adjusted_and_unadjusted_CI.R")
+source("quantlets/q5_effect_size.R")
+source("quantlets/q6_orthogonal_polynomial_contrasts.R")
+source("quantlets/q7_simulate_one_way_rmANOVA_data.R")
 
 set.seed(1)
 # Simulate data for report and presentation
 means = c(400, 170, 55, 45, 40)
 
 
-ow_rma_data = sim_ow_rma_data(n = 30, k = 5, means = means, poly_order = NULL, noise_sd = c(155, 65, 75, 15, 40), between_subject_sd = 60, NAs = 2) 
-ow_rma_data[,2:5] = (ow_rma_data[,2:5]  + 100) / 2
+rma_data = sim_rma_data(n = 30, k = 5, means = means, poly_order = NULL, noise_sd = c(155, 65, 75, 15, 40), between_subject_sd = 60, NAs = 2) 
+rma_data[,2:5] = (rma_data[,2:5]  + 100) / 2
 #ow_rma_data = read.csv("r_code/simulated_rigelmann_data.csv")
-ow_rma(ow_rma_data)
-ow_rma_ci(ow_rma_data, C_level = 0.99)
-ow_rma_eta(ow_rma_data)
-ow_rma_opc(ow_rma_data)
-ow_rma_spheri(ow_rma_data, append = TRUE)
-ow_rma_sse_reduct(ow_rma_data, ow_a_table = TRUE)
+rma(rma_data)
+rma_ci(rma_data, C_level = 0.99)
+rma_eta(rma_data)
+rma_opc(rma_data)
+rma_spheri(rma_data, append = TRUE)
+rma_sse_reduct(rma_data, return_anova_table = TRUE)
 
 
 #rma_data = sim_rma_data(n = 30, k = 5, means = means, poly_order = NULL, noise_sd = c(155, 65, 75, 15, 40), between_subject_sd = 60, NAs = 2) 
 #rma_data = (rma_data  + 100) / 2
-rma_data = read.csv(file = "r_code/simulated_rigelmann_data.csv")[,-2]
+rma_data = read.csv(file = "simulated_rigelmann_data.csv")[,-2]
 rma_data[,2:6] = (rma_data[,2:6]  + 100) / 2
 
 # rma function
@@ -38,7 +39,7 @@ xtable(rma_table$rm_ANOVA_table)
 # confidence intervals
 cis = rma_ci(rma_data, C_level = 0.99, print_plot = FALSE)
 xtable(cis$confidence_intervals)
-ggsave(file = "ci_plot.jpg", plot = cis$ci_plot, device = "jpg")
+ggsave(file = "ci_plot.pdf", plot = cis$ci_plot, device = "jpg")
 
 # eta function
 etas = rma_eta(rma_data)
@@ -56,7 +57,9 @@ xtable(spher$correction_factors_epsilon_table)
 # comparison of SSEs
 sse = rma_sse_reduct(rma_data)
 xtable(sse$sse_reduction_table)
->>>>>>> 7b3d28f8e5762acf8277d5e5e7f09a4c417d7ba2
+#?# >>>>>>> 7b3d28f8e5762acf8277d5e5e7f09a4c417d7ba2
 #write.csv(x = ow_rma_data, file = "r_code/simulated_rigelmann_data.csv")
 
-View(ow_rma_data)
+# Data structure
+data_structure = head(rma_data)
+xtable(data_structure)
